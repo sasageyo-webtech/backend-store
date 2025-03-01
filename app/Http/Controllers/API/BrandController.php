3ddef46120ca7029561d4b6b82c14f9fffe0995a
FLUKKIES <mootoonzerza@gmail.com>
2025-03-02 03:12:47 +0700

    add brand api to getAll, show, update, delete

---
+++ app/Http/Controllers/API/BrandController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BrandCollection;
+use App\Http\Resources\BrandResource;
+use App\Models\Brand;
+use App\Repositories\BrandRepository;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    public function __construct(
+        private BrandRepository $brandRepository
+    ){}
+
+    public function index()
+    {
+        $brands = $this->brandRepository->getAll();
+        return new BrandCollection($brands);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Brand $brand)
+    {
+        return new BrandResource($brand);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Brand $brand)
+    {
+        // TODO Validate update brand
+
+        $this->brandRepository->update([
+            'name' => $request->get('name'),
+        ], $brand->id);
+
+        return new BrandResource($brand->refresh());
+    }
+
+    public function destroy(Brand $brand)
+    {
+        $this->brandRepository->delete($brand->id);
+    }
+}
