79225816493dc6f25d97ffa25b0b598b62bb825a
FLUKKIES <mootoonzerza@gmail.com>
2025-03-01 22:41:53 +0700

    implement product api delete, show, update

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -27,7 +27,30 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //TODO Validate store product
+
+        $imagePaths = [];
+        if ($request->hasFile('image_paths')) {
+            foreach ($request->file('image_paths') as $file) {
+                $filename = time() . '-' . $file->getClientOriginalName();
+                $path = $file->storeAs('products', $filename, 'public');
+                $imagePaths[] = $path; // Store the path for each uploaded image
+            }
+        }
+
+        $product = $this->productRepository->create([
+            'category_id' => $request->get('category_id'),
+            'brand_id' => $request->get('brand_id'),
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'price' => $request->get('price'),
+            'stock' => $request->get('stock'),
+            'image_paths' => json_encode($imagePaths),
+            'rating' => $request->get('rating'),
+            'accessibility' => $request->get('accessibility'),
+        ]);
+
+        return new ProductResource($product);
     }
 
     /**
@@ -35,7 +58,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return new ProductResource($product);
     }
 
     /**
@@ -43,14 +66,36 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        //TODO Gate update product
+        //TODO Validate update product
+
+        if ($request->hasFile('image_paths')) {
+            foreach ($request->file('image_paths') as $file) {
+                $filename = time() . '-' . $file->getClientOriginalName();
+                $path = $file->storeAs('products', $filename, 'public');
+                $imagePaths[] = $path; // Add new image path to the array
+            }
+        }
+
+        $this->productRepository->update([
+            'category_id' => $request->get('category_id'),
+            'brand_id' => $request->get('brand_id'),
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'price' => $request->get('price'),
+            'stock' => $request->get('stock'),
+            'image_paths' => json_encode($imagePaths),
+            'rating' => $request->get('rating'),
+            'accessibility' => $request->get('accessibility'),
+        ], $product->id);
+
+        return new ProductResource($product->refresh());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Product $product)
     {
-        //
+        $id = $product->id;
+        //TODO Delete product when is not used in order
+        $product->delete();
     }
 }
