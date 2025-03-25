a767c1dcdee18d46fd30ecbdcdf264eb88bc787f
FLUKKIES <mootoonzerza@gmail.com>
2025-03-26 03:48:49 +0700

    fix status order

--- app/Models/Enums/OrderStatus.php
+++ app/Models/Enums/OrderStatus.php
@@ -4,7 +4,8 @@ namespace App\Models\Enums;
 
 enum OrderStatus: string
 {
-    case PENDING = 'PENDING';
-    case SUCCESS = 'SUCCESS';
-
+    case RECEIVED = ' RECEIVED'; // ได้รับ order, เตรียม pack ของ
+    case DELIVERED = ' DELIVERED'; // จัดส่งแล้ว
+    case SUCCESS = 'SUCCESS'; // รับสินค้าแล้ว
+    case FAIL = 'FAIL'; // ไม่ได้รับสินค้า
 }
